217a2b0fb83fffec5db7f6f8aca2944ddc160d49
2mf8 <chun5060@outlook.com>
2022-07-04 19:27:12 +0800

    add sendMsg

--- bot.go
+++ bot.go
@@ -469,3 +469,21 @@ func (bot *Bot) SetGroupSignIn(groupId int64) (*onebot.SetGroupSignInResp, error
 		return resp.GetSetGroupSignInResp(), nil
 	}
 }
+
+func (bot *Bot) SendMsg(groupId, userId int64, msg *Msg, autoEscape bool)(*onebot.SendMsgResp, error){
+	if resp, err := bot.sendFrameAndWait(&onebot.Frame{
+		FrameType: onebot.Frame_TSendMsgReq,
+		Data: &onebot.Frame_SendMsgReq{
+			SendMsgReq: &onebot.SendMsgReq{
+				UserId: userId,
+				GroupId: groupId,
+				Message: msg.MessageList,
+				AutoEscape: autoEscape,
+			},
+		},
+	}); err != nil {
+		return nil, err
+	} else {
+		return resp.GetSendMsgResp(), nil
+	}
+}
